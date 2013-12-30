50c9e162b8ea72758b38a3f5719f250f9bebf874
Kevin Mann <kevin@kevin.(none)>
2013-12-30 16:12:02 -0500

    Changing the port

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var path = require('path');
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8330);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 app.use(express.favicon());
